4e67ac2dd76054577d60fa73355853bf18bab9df
Black_star22 <blackstar7608@gmail.com>
2022-10-01 13:15:28 +0530

    Car game

---
+++ cargame.js
@@ -0,0 +1,15 @@
+// let car_status = "stop";
+// const input = require("readline-sync")
+// console.log("Enter help to see options.");
+// while(true){
+//     b = input.question(">>>>")
+//     if (car_status!=="start" && b==="start") console.log("Car is started"),car_status="start"
+//     else if (car_status==="start" && b==="start") console.log("Car is already started");
+//     else if (car_status!=="stop" && b==="stop") console.log("Car is stopped"),car_status="stop";
+//     else if (car_status==="stop" && b==="stop") console.log("Car is already stopped");
+//     else if (b=="help") console.log("ENTER (start/stop) to run.");
+//     else if (b==="exit") break
+//     else console.log("Command! not found");
+// }
+
+
